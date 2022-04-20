2055df66dd796c1646bacf4d8fcc746b1c04799f
omrobles <omrobles.dwfc4@ucamp.utel.edu.mx>
2022-04-20 08:37:01 -0500

    Creacion de navbar y footer con ruta creadas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavbarComp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola desde App.js</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/habitaciones" element={<Rooms />} />
+        <Route path="/actividades" element={<Activities />} />
+        <Route path="/servicios" element={<Services />} />
+        <Route path="/contacto" element={<Contact />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+function Home() {
+  return <h1>Hola desde Home</h1>;
+}
+
+function Rooms() {
+  return <h1>Hola desde rooms</h1>;
+}
+
+function Activities() {
+  return <h1>Hola desde Activities</h1>;
+}
+
+function Services() {
+  return <h1>Hola desde servicios</h1>;
+}
+
+function Contact() {
+  return <h1>Hola desde contacto</h1>;
+}
