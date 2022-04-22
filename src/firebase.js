228e929584d835181f3ed5c58cc356ccdaa9cf62
omrobles <omrobles.dwfc4@ucamp.utel.edu.mx>
2022-04-22 12:50:54 -0500

    Se arregla conexion a firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,18 +5,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyDWCgndtixHOJcMkFavUsALpqv6HPwBZ6I",
-  // authDomain: "hotel-app-12301.firebaseapp.com",
-  // projectId: "hotel-app-12301",
-  // storageBucket: "hotel-app-12301.appspot.com",
-  // messagingSenderId: "648305765098",
-  // appId: "1:648305765098:web:08e48ce75790bbeceac593"
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERI,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  apiKey: "AIzaSyDWCgndtixHOJcMkFavUsALpqv6HPwBZ6I",
+  authDomain: "hotel-app-12301.firebaseapp.com",
+  projectId: "hotel-app-12301",
+  storageBucket: "hotel-app-12301.appspot.com",
+  messagingSenderId: "648305765098",
+  appId: "1:648305765098:web:08e48ce75790bbeceac593"
+  // apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERI,
+  // appId: process.env.REACT_APP_FIREBASE_APPID
 };
 
 // Initialize Firebase
